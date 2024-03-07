14064fbb339654ec2f8d1d436c6b345c9d23c07f
Josh Arbon <jarbon@student.neumont.edu>
2024-03-06 17:48:56 -0700

    move to pc

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
-// var dadJoke = document.getElementById("dad-joke")
+var dadJoke = document.getElementById("dad-joke")
 
-// fetch('https://icanhazdadjoke.com/', {
-//     headers: {
-//         Accept: 'application/json'
-//     }
-// }).then(response => {
-//     response.json().then(data => {
-//         var joke = data.joke
-//         dadJoke.textContent = joke
-//     })
-// })
+fetch('https://icanhazdadjoke.com/', {
+    headers: {
+        Accept: 'application/json'
+    }
+}).then(response => {
+    response.json().then(data => {
+        var joke = data.joke
+        dadJoke.textContent = joke
+    })
+})
